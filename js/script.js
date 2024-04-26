e919856ab804e723e57484393308246c53c30798
Fethawit <159185142+fetuha-gebremekeal@users.noreply.github.com>
2024-04-26 18:27:02 +0000

    js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
+// Copyright (c) 2024 Fetuha All rights reserved
+//
 // Created by: Fetuha
-// Created on: Apr 2024
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
 "use strict"
 
@@ -7,10 +9,11 @@ function alwaysChecked() {
   const optionPositive = (document.getElementById("option-positive").checked)
 
   if (optionPositive == true) {
-    const pRand = Math.floor(Math.random() * 6) + 1
-    document.getElementById("result").innerHTML = "Your number is: " + pRand
+    const pRand = Math.floor(Math.random() * 9) + 1
+    document.getElementById("answer").innerHTML = "Your number is: " + pRand
   } else {
-    const nRand = Math.floor(Math.random() * 6) + 1
-    document.getElementById("result").innerHTML = "Your number is: " + nRand
+    const nRand = Math.floor((Math.random() * 9) + 1) * -1
+    document.getElementById("answer").innerHTML = "Your number is: " + nRand
   }
+  // Output the answer to the user in the class "answer"
 }
